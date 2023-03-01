0cce24dc76b8cb3d9c7f9a91a18bda5a24748ec2
Gavin Uhran <gavinu2001@gmail.com>
2023-02-28 21:04:04 -0500

    Created basic Express server

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+// import bodyParser from 'body-parser';
+// import mongoose from 'mongoose';
+import dotenv from 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+  
+app.get('/', async (req, res, next) => {
+    try {
+        res.send('Hello world');
+    } catch (err) {
+        res.status(500).send({error: 'Internal server error' });
+    }
+})
+
+// const MONGO_DB_URI = process.env.MONGO_DB_URI;
+
+// mongoose.connect(MONGO_DB_URI, { 
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true 
+// });
+
+// mongoose.connection.on('open', () => {
+//     console.log('Connected to MongoDB database.');
+// });
+
+app.listen(PORT, () => {
+    console.log(`Briefly server listening on port ${PORT}`);
+})
